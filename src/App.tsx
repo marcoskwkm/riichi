a903b59113630b41d38d57418d80b10d9b7b6e66
Marcos Kawakami <marcoskwkm@gmail.com>
2021-01-30 00:33:40 -0300

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
+} from 'react-router-dom'
+
+import ScoreCalc from './pages/score-calc'
 
 const App: React.FC = () => (
-  <div className="flex justify-center">
-    <div className="m-4 p-4 border border-solid border-black f3 text-red-500 text-center w-1/2">
-      Hello, world!
-    </div>
-  </div>
+  <Router basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/score-calc" />
+      </Route>
+      <Route path="/score-calc">
+        <ScoreCalc />
+      </Route>
+    </Switch>
+  </Router>
 )
 
 export default App
---
+++ src/pages/score-calc/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ScoreCalc: React.FC = () => (
+  <div className="flex justify-center">
+    <div className="m-4 p-4 border border-solid border-black f3 text-red-500 text-center w-1/2">
+      Hello, world!
+    </div>
+  </div>
+)
+
+export default ScoreCalc
