c55c69a0d5eb987cbabbf81e4d3e1a0229a9c9f2
Marcos Kawakami <marcoskwkm@gmail.com>
2021-01-30 00:26:47 -0300

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
-const App: React.FC = () => <div>Hello, world!</div>
+const App: React.FC = () => (
+  <div className="flex justify-center">
+    <div className="m-4 p-4 border border-solid border-black f3 text-red-500 text-center w-1/2">
+      Hello, world!
+    </div>
+  </div>
+)
 
 export default App
