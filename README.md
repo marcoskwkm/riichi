08bf31ca6c8b21eaa96d6196a7f5785047ed16cc
Marcos Kawakami <marcoskwkm@gmail.com>
2021-02-02 22:14:27 -0300

    [ScoreCalc] Add option to practice only on common fu values (<= 40)

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Riichi training tools
 
-Link [https://marcoskwkm.github.io/riichi](https://marcoskwkm.github.io/riichi)
+Link: [https://marcoskwkm.github.io/riichi](https://marcoskwkm.github.io/riichi)
 
 So far this only has a tool to help you practice calculating hand scores given han and fu values. I will probably add more stuff as I feel like it.
+
+## Score Trainer
+
+The box gives you the information you need to calculate the hand score. Type the hand payments in the input box in the form `[non-dealer payment]/[dealer payment]` in case of non-dealer tsumo, or in the form `[player payment]` otherwise. For example, for a 1 han 30 fu non-dealer ron the answer is `1000`, and for 3 han 25 fu non-dealer tsumo the answer is `800/1600`.
+
+Type in your answer and press enter. If your answer is right you'll be given another prompt. Otherwise, you can try again. In case you get it wrong the expected answer will be printed in the developer console (too lazy to actually put it on the interface), so you can double check if your memory/calculation was correct.
--- src/components/ScoreCalcTrainer/index.tsx
+++ src/components/ScoreCalcTrainer/index.tsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import ScoreCalcPrompt from '../ScoreCalcPrompt'
 import { ALL_SCORES } from './constants'
 
-const getRandomHand = () => {
-  const idx = Math.floor(ALL_SCORES.length * Math.random())
-  return ALL_SCORES[idx]
+const isCommonHand = (hand: HandScore) => !hand.fu || hand.fu <= 40
+
+const getRandomHand = (options: { common?: boolean } = {}) => {
+  const pool = options.common
+    ? ALL_SCORES.filter((hand) => isCommonHand(hand))
+    : ALL_SCORES
+
+  const idx = Math.floor(pool.length * Math.random())
+  return pool[idx]
 }
 
 const ScoreCalcTrainer: React.FC = () => {
   const [hand, setHand] = useState<HandScore>(getRandomHand())
   const [guess, setGuess] = useState<string>('')
+  const [commonOnly, setCommonOnly] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (commonOnly && !isCommonHand(hand)) {
+      setHand(getRandomHand({ common: true }))
+    }
+  }, [commonOnly, hand])
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key !== 'Enter') {
@@ -18,7 +31,7 @@ const ScoreCalcTrainer: React.FC = () => {
     }
 
     if (guess === hand?.score) {
-      setHand(getRandomHand())
+      setHand(getRandomHand({ common: commonOnly }))
       setGuess('')
     } else {
       console.log('Wrong. Answer:', hand?.score)
@@ -28,8 +41,17 @@ const ScoreCalcTrainer: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
       <ScoreCalcPrompt hand={hand} />
+      <label className="mt-4">
+        <input
+          className="mr-2"
+          type="checkbox"
+          checked={commonOnly}
+          onChange={(event) => setCommonOnly(event.target.checked)}
+        />
+        40 fu or less only
+      </label>
       <input
-        className="mt-8 w-56 px-1 border border-gray-400 rounded text-center"
+        className="mt-4 w-56 px-1 border border-gray-400 rounded text-center"
         value={guess}
         onChange={(event) => setGuess(event.target.value)}
         onKeyDown={handleKeyDown}
