c31341c6fb3969d7eb3f54c4b84cdcdb71e3982b
Marcos Kawakami <marcoskwkm@gmail.com>
2023-05-04 11:54:11 -0300

    [ScoreCalc] Add option to limit hands to <= 4 han only

--- src/pages/score-calc/components/ScoreCalcTrainer/index.tsx
+++ src/pages/score-calc/components/ScoreCalcTrainer/index.tsx
@@ -4,11 +4,20 @@ import ScoreCalcPrompt from '../ScoreCalcPrompt'
 import { ALL_SCORES } from './constants'
 
 const isCommonHand = (hand: HandScore) => !hand.fu || hand.fu <= 50
+const isLowHanHand = (hand: HandScore) => hand.han <= 4
 
-const getRandomHand = (options: { common?: boolean } = {}) => {
-  const pool = options.common
-    ? ALL_SCORES.filter((hand) => isCommonHand(hand))
-    : ALL_SCORES
+const getRandomHand = (
+  options: { common?: boolean; lowHan?: boolean } = {}
+) => {
+  let pool = ALL_SCORES
+
+  if (options.common) {
+    pool = pool.filter(isCommonHand)
+  }
+
+  if (options.lowHan) {
+    pool = pool.filter(isLowHanHand)
+  }
 
   const idx = Math.floor(pool.length * Math.random())
   return pool[idx]
@@ -18,12 +27,16 @@ const ScoreCalcTrainer: React.FC = () => {
   const [hand, setHand] = useState<HandScore>(getRandomHand())
   const [guess, setGuess] = useState<string>('')
   const [commonOnly, setCommonOnly] = useState<boolean>(true)
+  const [lowHanOnly, setLowHanOnly] = useState<boolean>(true)
 
   useEffect(() => {
-    if (commonOnly && !isCommonHand(hand)) {
-      setHand(getRandomHand({ common: true }))
+    if (
+      (commonOnly && !isCommonHand(hand)) ||
+      (lowHanOnly && !isLowHanHand(hand))
+    ) {
+      setHand(getRandomHand({ common: commonOnly, lowHan: lowHanOnly }))
     }
-  }, [commonOnly, hand])
+  }, [commonOnly, lowHanOnly, hand])
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key !== 'Enter') {
@@ -41,15 +54,26 @@ const ScoreCalcTrainer: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
       <ScoreCalcPrompt hand={hand} />
-      <label className="mt-4">
-        <input
-          className="mr-2"
-          type="checkbox"
-          checked={commonOnly}
-          onChange={(event) => setCommonOnly(event.target.checked)}
-        />
-        50 fu or less only
-      </label>
+      <div className="mt-4 flex items-center gap-x-4">
+        <label>
+          <input
+            className="mr-2"
+            type="checkbox"
+            checked={commonOnly}
+            onChange={(event) => setCommonOnly(event.target.checked)}
+          />
+          50 fu or less only
+        </label>
+        <label>
+          <input
+            className="mr-2"
+            type="checkbox"
+            checked={lowHanOnly}
+            onChange={(event) => setLowHanOnly(event.target.checked)}
+          />
+          4 han or less only
+        </label>
+      </div>
       <input
         className="mt-4 w-56 rounded border border-gray-400 px-1 text-center"
         value={guess}
